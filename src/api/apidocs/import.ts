7f55aa22f1b80b7372ac932d5a83f9524a38d401
vishnuprashob-sathish <vishnuprashob.sathish@kovai.co>
2023-07-07 14:39:57 +0530

    fix: added validation for servers in a specFile

--- src/api/apidocs/import.ts
+++ src/api/apidocs/import.ts
@@ -3,7 +3,7 @@ import FormData from "form-data";
 import fs from "fs";
 import { Headers } from "node-fetch";
 import ora from "ora";
-import { error, success } from "../../helper/consoleWrapper";
+import { error, success, warning } from "../../helper/consoleWrapper";
 import { getCIName } from "../../helper/isCI";
 import { isURL } from "../../helper/isURL";
 import { ApiReferenceOperationType, ApiReferenceSourceType, ImportCommandOptions } from "../../models";
@@ -12,57 +12,79 @@ import { ImportApiReferenceSummary } from "../../models/import.model";
 import d360APIFetch from "../d360APIFetch";
 
 export default async function importFlow(options: ImportCommandOptions) {
-    const spinner = ora({ text: "Importing.." });
-    spinner.start();
-    const formData = getImportRequestFormData(options);
-    const requestOptions = {
-        method: "POST",
-        body: formData,
-        headers: new Headers({
-            api_token: options.apiKey,
-        }),
-    };
-    let requestUrl = `/v2/apidocs/import/${options.userId}`;
-    if (options.publish) {
-        requestUrl += `/?publishArticles=true`;
-    }
-    const response = await d360APIFetch<ApiResponse<ImportApiReferenceSummary>>(requestUrl, requestOptions);
-    spinner.stop();
-    handleImportResponse(response);
+  const spinner = ora({ text: "Importing.." });
+  spinner.start();
+  const formData = getImportRequestFormData(options);
+  const requestOptions = {
+    method: "POST",
+    body: formData,
+    headers: new Headers({
+      api_token: options.apiKey,
+    }),
+  };
+  let requestUrl = `/v2/apidocs/import/${options.userId}`;
+  if (options.publish) {
+    requestUrl += `/?publishArticles=true`;
+  }
+  const response = await d360APIFetch<ApiResponse<ImportApiReferenceSummary>>(requestUrl, requestOptions);
+  spinner.stop();
+  handleImportResponse(response);
 }
 
 function getImportRequestFormData(options: ImportCommandOptions) {
-    const formData = new FormData();
-    if (!isURL(options.path)) {
-        const stream = fs.createReadStream(options.path);
-        formData.append("file", stream);
-    } else {
-        formData.append("url", options.path);
-    }
-    formData.append("projectVersionId", options.versionId);
-    formData.append("sourceType", ApiReferenceSourceType.CommandLine);
-    formData.append("operationType", ApiReferenceOperationType.Import);
-    formData.append("ciName", getCIName() ?? "command-line");
-    if (options.force) {
-        formData.append("proceedAnyway", "true");
-    }
-    return formData;
+  const formData = new FormData();
+  if (!isURL(options.path)) {
+    const stream = fs.createReadStream(options.path);
+    formData.append("file", stream);
+  } else {
+    formData.append("url", options.path);
+  }
+  formData.append("projectVersionId", options.versionId);
+  formData.append("sourceType", ApiReferenceSourceType.CommandLine);
+  formData.append("operationType", ApiReferenceOperationType.Import);
+  formData.append("ciName", getCIName() ?? "command-line");
+  if (options.force) {
+    formData.append("proceedAnyway", "true");
+  }
+  return formData;
 }
 
 function handleImportResponse(response: ApiResponse<ImportApiReferenceSummary>) {
-    if (response?.success) {
-        const importWarnings = response?.result?.warnings;
-        const importErrors = response?.result?.errors;
-        if ((importErrors == null && importWarnings == null)  ||  (importErrors.length <= 0 && importWarnings.length <= 0)) {
-          success("Import Successful!");
-        } else if (importWarnings.length > 0 && importErrors.length > 0) {
-          error(
-            `Import failed! We found ${importWarnings.length} alert(s) and ${importErrors.length} errors. You can use --force to force import your spec file`
-          );
-        } else if (importWarnings.length > 0) {
-          error(`Import failed! We found ${importWarnings.length} alert(s). You can use --force to force import your spec file`);
-        } else if (importErrors.length > 0) {
-          error(`Import failed! We found ${importErrors.length} errors. You can use --force to force import your spec file`);
-        }
+  if (response?.success) {
+    const importWarnings = response?.result?.warnings;
+    const importErrors = response?.result?.errors;
+    const isServersAvailable = response?.result?.is_server_available;
+    const apiDefinitionImported = response?.result?.api_definition_id != null ? true : false;
+    if (importErrors == null && importWarnings == null && apiDefinitionImported == true) {
+      success("Import Successful!");
+    } else if (isServersAvailable != true && importWarnings.length < 1 && importErrors.length < 1) {
+      showServerUnAwailableWarning(true);
+    } else if (importWarnings.length > 0 && importErrors.length > 0) {
+      error(
+        `Import failed! We found ${importWarnings.length} alert(s) and ${importErrors.length} errors. You can use --force to force import your spec file`
+      );
+      if (isServersAvailable == false) {
+        showServerUnAwailableWarning(false);
+      }
+    } else if (importWarnings.length > 0) {
+      error(`Import failed! We found ${importWarnings.length} alert(s). You can use --force to force import your spec file`);
+      if (isServersAvailable == false) {
+        showServerUnAwailableWarning(false);
+      }
+    } else if (importErrors.length > 0) {
+      error(`Import failed! We found ${importErrors.length} errors. You can use --force to force import your spec file`);
+      if (isServersAvailable == false) {
+        showServerUnAwailableWarning(false);
+      }
     }
+  }
+}
+function showServerUnAwailableWarning(withForceInfo: boolean) {
+  if (withForceInfo) {
+    warning(
+      "You may not be able to use try-it feature. Looks like server variables aren’t defined in your OpenAPI Specification file. You can use --force to force import your spec file"
+    );
+  } else {
+    warning("You may not be able to use try-it feature. Looks like server variables aren't defined in your OpenAPI Specification file.");
+  }
 }
--- src/models/import.model.ts
+++ src/models/import.model.ts
@@ -1,16 +1,16 @@
 export interface ImportApiReferenceSummary {
-    rootCategoryId: string;
-    projectId: string;
-    projectDocumentVersionId: string;
-    categoriesCreated: number;
-    articlesCreated: number;
-    isApiDefinitionImported: boolean;
-    errors: LogMessageAndPointer[];
-    warnings: LogMessageAndPointer[];
-    isSuccess: boolean;
-    apiDefinitionsCount: number;
-    apiDefinitionId?: string;
-    isServerAvailable: boolean;
+  rootCategoryId: string;
+  projectId: string;
+  projectDocumentVersionId: string;
+  categoriesCreated: number;
+  articlesCreated: number;
+  isApiDefinitionImported: boolean;
+  errors: LogMessageAndPointer[];
+  warnings: LogMessageAndPointer[];
+  is_successs: boolean;
+  apiDefinitionsCount: number;
+  api_definition_id?: string;
+  is_server_available: boolean;
 }
 
 export interface LogMessageAndPointer {
